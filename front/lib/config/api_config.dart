1970e2dd0307516ae233994596c1f535c2e87d2f
Spandiyar <fajzulaevspandiar@gmail.com>
2025-11-07 00:11:27 +0500

    prod2

--- front/lib/config/api_config.dart
+++ front/lib/config/api_config.dart
@@ -1,3 +1,3 @@
 class ApiConfig {
-  static const String baseUrl = 'http://ortusbrand-production.up.railway.app/api';
+  static const String baseUrl = 'http://ortusbrand-production-0ace.up.railway.app/api';
 }
